f261665c6c0db9fdb7dfca8ecf492990ff8effb3
datWaffle21 <33697339+datWaffle21@users.noreply.github.com>
2024-01-11 00:16:19 -0600

    added score and rounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+    private int score = 1;
+    private int round = 1;
+    private float delta = 0;
+
+    public Text roundText;
+    public Text scoreText;
     
-    
+    void UpdateHUD() {
+        roundText.text = "Round: " + round;
+        scoreText.text = "Score: " + score;
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -13,8 +24,18 @@ public class GameManager : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        delta += Time.deltaTime;
+        if (delta > .25f) {
+            score++;
+            delta = 0f;
+        }
+
+        if (score % 90 == 0) {
+            round++;
+            score++;
+        }
         
+        UpdateHUD();
     }
 }
